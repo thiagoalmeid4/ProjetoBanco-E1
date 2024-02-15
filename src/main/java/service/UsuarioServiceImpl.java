17302541ec2245c3f1cf60148db412ed8b07e439
davisoles <drbp.1306@gmail.com>
2024-02-15 17:51:19 -0300

    Finalizado classe usuario service

--- src/main/java/service/UsuarioServiceImpl.java
+++ src/main/java/service/UsuarioServiceImpl.java
@@ -1,7 +1,10 @@
 package service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.InputMismatchException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import dao.UsuarioDao;
 import models.Usuario;
@@ -17,6 +20,7 @@ public class UsuarioServiceImpl implements UsuarioService{
 	@Override
 	public void salvarUsuario(Usuario usuario) {
 		validarUsuario(usuario);
+		
 	}
 
 	@Override
@@ -40,25 +44,93 @@ public class UsuarioServiceImpl implements UsuarioService{
 	
 	private void validarUsuario(Usuario usuario) {
 		
+		validarDataNascimento(usuario.getDataNascimento());
+		validarCpf(usuario.getCpf());
+		validarEmail(usuario.getEmail());
+		
 		if(usuario.getNome().isBlank()) {
 			throw new RuntimeException("Nome deve ser informado.");
 		}
-		//finalizar essa verificação
-		if(usuario.getCpf().length() != 11) {
-			throw new RuntimeException("Cpf inválido.");
-		}
-		//finalizar tb
-		if(usuario.getEmail().isBlank()) {
-			throw new RuntimeException("Email invalido.");
-		}
+		
 		if(usuario.getSenha().length() < 6) {
-			throw new RuntimeException("Senha tem que ter mais que 6 dígitos.");
+			throw new RuntimeException("Senha deve conter 6 ou mais caracteres.");
 		}
-		
-		
-		
 	}
 	
+	private void validarDataNascimento(LocalDate dataNascimento) {
+        LocalDate hoje = LocalDate.now();
+        Period periodo = Period.between(dataNascimento, hoje);
+        int idade = periodo.getYears();
+
+        if (idade > 90) {
+            throw new RuntimeException("Data de nascimento inválida");
+        }
+        if(idade < 18) {
+        	throw new RuntimeException("O usuário deve ter 18 anos ou mais");
+        }
+    }
 	
+	private void validarCpf(String CPF) {
+	    // considera-se erro CPF's formados por uma sequencia de numeros iguais
+	    if (CPF.equals("00000000000") ||
+	        CPF.equals("11111111111") ||
+	        CPF.equals("22222222222") || CPF.equals("33333333333") ||
+	        CPF.equals("44444444444") || CPF.equals("55555555555") ||
+	        CPF.equals("66666666666") || CPF.equals("77777777777") ||
+	        CPF.equals("88888888888") || CPF.equals("99999999999") ||
+	        (CPF.length() != 11))
+	        throw new RuntimeException("CPF inválido!");
+
+	    char dig10, dig11;
+	    int sm, i, r, num, peso;
+
+	    // Calculo do 1o. Digito Verificador
+	    sm = 0;
+	    peso = 10;
+	    for (i = 0; i < 9; i++) {
+	        // converte o i-esimo caractere do CPF em um numero:
+	        // por exemplo, transforma o caractere "0" no inteiro 0
+	        // (48 eh a posicao de "0" na tabela ASCII)
+	        num = (int) (CPF.charAt(i) - 48);
+	        sm = sm + (num * peso);
+	        peso = peso - 1;
+	    }
+
+	    r = 11 - (sm % 11);
+	    if ((r == 10) || (r == 11))
+	        dig10 = '0';
+	    else
+	        dig10 = (char) (r + 48); // converte no respectivo caractere numerico
+
+	    // Calculo do 2o. Digito Verificador
+	    sm = 0;
+	    peso = 11;
+	    for (i = 0; i < 10; i++) {
+	        num = (int) (CPF.charAt(i) - 48);
+	        sm = sm + (num * peso);
+	        peso = peso - 1;
+	    }
+
+	    r = 11 - (sm % 11);
+	    if ((r == 10) || (r == 11))
+	        dig11 = '0';
+	    else
+	        dig11 = (char) (r + 48);
+
+	    // Verifica se os digitos calculados conferem com os digitos informados.
+	    if (dig10 != CPF.charAt(9) || dig11 != CPF.charAt(10))
+	        throw new RuntimeException("CPF inválido!");
+	}
+
+	
+	private void validarEmail(String email) {
+		
+        // Expressão regular para validar o formato do e-mail
+        String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        
+        if (!email.matches(regex)) {
+        	throw new RuntimeException("Email inválido!");
+        }
+    }
 
 }
