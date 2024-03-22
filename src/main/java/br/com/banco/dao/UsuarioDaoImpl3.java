1380df47775e7949e21e894762a0f3565f5ae526
EduardoBabetto <edubabetto@gmail.com>
2024-03-22 16:37:36 -0300

    Corrigindo conflito

--- src/main/java/br/com/banco/dao/UsuarioDaoImpl3.java
+++ src/main/java/br/com/banco/dao/UsuarioDaoImpl3.java
@@ -1,33 +1,33 @@
-/*package br.com.banco.dao;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import br.com.banco.dao.repository.UsuarioRepository;
-import br.com.banco.models.Usuario;
-
-//@Component
-public class UsuarioDaoImpl3 implements UsuarioDao {
-
-	@Autowired
-	UsuarioRepository usuarioRepository;
-	
-	@Override
-	public void salvar(Usuario usuario) {
-		usuarioRepository.save(usuario);
-		
-	}
-
-	@Override
-	public List<Usuario> listarTodos() {
-		return usuarioRepository.findAll();
-	}
-
-	@Override
-	public Usuario retornarPorID(long idUsuario) {
-		return usuarioRepository.findById(idUsuario).get();
-	}
-
-}*/
+//package br.com.banco.dao;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import br.com.banco.dao.repository.UsuarioRepository;
+//import br.com.banco.models.Usuario;
+//
+////@Component
+//public class UsuarioDaoImpl3 implements UsuarioDao {
+//
+//	@Autowired
+//	UsuarioRepository usuarioRepository;
+//	
+//	@Override
+//	public void salvar(Usuario usuario) {
+//		usuarioRepository.save(usuario);
+//		
+//	}
+//
+//	@Override
+//	public List<Usuario> listarTodos() {
+//		return usuarioRepository.findAll();
+//	}
+//
+//	@Override
+//	public Usuario retornarPorID(long idUsuario) {
+//		return usuarioRepository.findById(idUsuario).get();
+//	}
+//
+//}
