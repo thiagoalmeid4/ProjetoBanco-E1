7d1b7fd51431fcf6b2c4055106f76fbe365466d4
Yago Piovarczik <ypiovarczik@foursys.com.br>
2024-03-25 16:58:13 -0300

    Exportador conta e usuario

--- src/main/java/br/com/banco/utils/ExportadorConta.java
+++ src/main/java/br/com/banco/utils/ExportadorConta.java
@@ -1,5 +1,25 @@
 package br.com.banco.utils;
 
-public class ExportadorConta {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 
+import br.com.banco.models.Conta;
+public class ExportadorConta {
+	public static void exportarParaTxt(List<Conta> contas, String saida) {
+	try (BufferedWriter writer = new BufferedWriter(new FileWriter(saida))) {
+		for (Conta conta : contas) {
+		writer.write(conta.getIdConta() 
+		+ "-" + conta.getIdUsuario() 
+		+ "-" + conta.getNumeroConta()
+		+ "-" + conta.getAgencia()
+		+ "-" + conta.getSaldo()
+		+ "-" + conta.getLimiteCredito());
+		writer.newLine();
+		}
+		} catch (IOException e) {
+		e.printStackTrace();
+		}
+		}
 }
--- src/main/java/br/com/banco/utils/ExportadorUsuario.java
+++ src/main/java/br/com/banco/utils/ExportadorUsuario.java
@@ -1,5 +1,27 @@
 package br.com.banco.utils;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import br.com.banco.models.Usuario;
+
 
 public class ExportadorUsuario {
 
+	    public static void exportarParaTxt(List<Usuario> usuarios, String saida) {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(saida))) {
+			for (Usuario usuario : usuarios) {
+			writer.write(usuario.getIdUsuario() 
+			+ ";" + usuario.getNome()
+			+ ";" + usuario.getSenha()
+			+ ";" + usuario.getCpf()
+			+ ";" + usuario.getDataNascimento());
+			writer.newLine();
+			}
+			} catch (IOException e) {
+			e.printStackTrace();
+			}
+			}
 }
+
+
