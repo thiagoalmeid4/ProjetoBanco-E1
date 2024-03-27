82b54fbbe9b4329e04b72256921584154a60cab5
EduardoBabetto <edubabetto@gmail.com>
2024-03-27 18:09:51 -0300

    Feita modificacoes na geracao de transferencias

--- src/main/java/br/com/banco/dao/ContaDaoImpl4.java
+++ src/main/java/br/com/banco/dao/ContaDaoImpl4.java
@@ -11,56 +11,50 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import br.com.banco.connection.ConnectionJDBC;
-import br.com.banco.dao.repository.ContaRepository;
 import br.com.banco.models.Conta;
+
 @Repository
 public class ContaDaoImpl4 implements ContaDao {
-	
-	ContaRepository repository;
-	PreparedStatement desc;
-	Statement declaracao;
+
 	ResultSet resultado;
+
 	@Override
 	public void salvar(Conta conta) {
-		try {
-		Connection con = ConnectionJDBC.abrir();
-		desc=con.prepareStatement("INSERT INTO tb_conta VALUES (DEFAULT,(SELECT MAX(PK_ID_USUARIO) FROM TB_USUARIO),?,?,?,?)");
-		desc.setLong(1, conta.getIdUsuario());
-		desc.setBigDecimal(2,conta.getSaldo());
-		desc.setLong(3,conta.getAgencia());
-		desc.setLong(4, conta.getNumeroConta());
-		desc.setBigDecimal(5, conta.getLimiteCredito());
-		desc.execute();
-		desc.close();
-		con.close();
+		try(Connection con = ConnectionJDBC.abrir();
+				PreparedStatement desc = con.prepareStatement(
+						"INSERT INTO tb_conta VALUES (DEFAULT,(SELECT MAX(PK_ID_USUARIO) FROM TB_USUARIO),?,?,?,?)")) {
+			desc.setLong(1, conta.getIdUsuario());
+			desc.setBigDecimal(2, conta.getSaldo());
+			desc.setLong(3, conta.getAgencia());
+			desc.setLong(4, conta.getNumeroConta());
+			desc.setBigDecimal(5, conta.getLimiteCredito());
+			desc.execute();
+			desc.close();
+			con.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e.getMessage());
 		}
-		catch(SQLException e) {
-			e.getMessage();
-		}
-		
+
 	}
 
 	@Override
 	public List<Conta> listarTodos() {
 		List<Conta> contas = new ArrayList<>();
-		try {
-		Connection con = ConnectionJDBC.abrir();
-		declaracao =con.createStatement();
-		resultado = declaracao.executeQuery("SELECT * FROM tb_conta ");
-		while(resultado.next()) {
-			Conta conta = new Conta();
-			conta.setIdUsuario(resultado.getLong("fk_id_usuario"));
-			conta.setSaldo(resultado.getBigDecimal("NR_Saldo"));
-			conta.setAgencia(resultado.getShort("NR_agencia"));
-			conta.setNumeroConta(resultado.getInt("NR_numero_conta"));
-			conta.setLimiteCredito(resultado.getBigDecimal("NR_limite_credito"));
-			contas.add(conta);
-			declaracao.close();
-			con.close();
-		}
-		}
-		catch(SQLException e) {
-			e.getMessage();
+
+		try (Connection con = ConnectionJDBC.abrir();Statement declaracao = con.createStatement()) {
+			resultado = declaracao.executeQuery("SELECT * FROM tb_conta ");
+			while (resultado.next()) {
+				Conta conta = new Conta();
+				conta.setIdUsuario(resultado.getLong("fk_id_usuario"));
+				conta.setIdConta(resultado.getLong("pk_id_conta"));
+				conta.setSaldo(resultado.getBigDecimal("NR_Saldo"));
+				conta.setAgencia(resultado.getShort("NR_agencia"));
+				conta.setNumeroConta(resultado.getInt("NR_numero_conta"));
+				conta.setLimiteCredito(resultado.getBigDecimal("NR_limite_credito"));
+				contas.add(conta);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException (e.getMessage());
 		}
 		return contas;
 	}
@@ -68,85 +62,73 @@ public class ContaDaoImpl4 implements ContaDao {
 	@Override
 	public Conta retornarPorID(long idConta) {
 		Conta conta = new Conta();
-		try {
-		Connection con = ConnectionJDBC.abrir();
-		desc = con.prepareStatement("SELECT * FROM tb_conta WHERE id_conta = ?");
-		desc.setLong(1,idConta);
-		resultado = desc.executeQuery();
-		resultado.next();
-		conta.setIdUsuario(resultado.getLong("fk_id_usuario"));
-		conta.setSaldo(resultado.getBigDecimal("NR_saldo"));
-		conta.setAgencia(resultado.getShort("NR_Agencia"));
-		conta.setNumeroConta(resultado.getInt("NR_Numero_conta"));
-		conta.setLimiteCredito(resultado.getBigDecimal("NR_Limite_credito"));
-		desc.close();
-		con.close();
-		}
-		catch(SQLException e) {
-			e.getMessage();
+		try (Connection con = ConnectionJDBC.abrir()) {
+			PreparedStatement desc = con.prepareStatement("SELECT * FROM tb_conta WHERE id_conta = ?");
+			desc.setLong(1, idConta);
+			resultado = desc.executeQuery();
+			resultado.next();
+			conta.setIdUsuario(resultado.getLong("fk_id_usuario"));
+			conta.setSaldo(resultado.getBigDecimal("NR_saldo"));
+			conta.setAgencia(resultado.getShort("NR_Agencia"));
+			conta.setNumeroConta(resultado.getInt("NR_Numero_conta"));
+			conta.setLimiteCredito(resultado.getBigDecimal("NR_Limite_credito"));
+		} catch (SQLException e) {
+			throw new RuntimeException(e.getMessage());
 		}
 		return conta;
 	}
 
 	@Override
 	public void atualizar(Conta conta) {
-		
-		try {
-			Connection con = ConnectionJDBC.abrir();
-			desc = con.prepareStatement("UPDATE tb_conta SET nr_saldo=? ,nr_limite_credito=? WHERE id_conta = ?");
+
+		try (Connection con = ConnectionJDBC.abrir();
+				PreparedStatement desc = con
+						.prepareStatement("UPDATE tb_conta SET nr_saldo=? ,nr_limite_credito=? WHERE id_conta = ?")) {
 			desc.setBigDecimal(1, conta.getSaldo());
 			desc.setBigDecimal(2, conta.getLimiteCredito());
 			desc.setLong(3, conta.getIdConta());
 			desc.execute();
-			con.close();;
-		}
-		catch(SQLException e) {
-			e.getMessage();
+		} catch (SQLException e) {
+			throw new RuntimeException(e.getMessage());
 		}
 	}
+
 	@Override
-	public Conta retornarPorAgenciaNum(long agencia,long numero) {
+	public Conta retornarPorAgenciaNum(long agencia, long numero) {
 		Conta conta = new Conta();
-		try {
-		Connection con = ConnectionJDBC.abrir();
-		desc = con.prepareStatement("SELECT * FROM tb_conta WHERE NR_AGENCIA = ? AND NR_NUMERO_CONTA = ?");
-		desc.setLong(1,agencia);
-		desc.setLong(2, numero);		
-		resultado = desc.executeQuery();
-		resultado.next();
-		conta.setIdUsuario(resultado.getLong("fk_id_usuario"));
-		conta.setSaldo(resultado.getBigDecimal("NR_saldo"));
-		conta.setAgencia(resultado.getShort("NR_Agencia"));
-		conta.setNumeroConta(resultado.getInt("NR_Numero_conta"));
-		conta.setLimiteCredito(resultado.getBigDecimal("NR_Limite_credito"));
-		desc.close();
-		con.close();
-		}
-		catch(SQLException e) {
-			e.getMessage();
+		try (Connection con = ConnectionJDBC.abrir();
+				PreparedStatement desc = con
+						.prepareStatement("SELECT * FROM tb_conta WHERE NR_AGENCIA = ? AND NR_NUMERO_CONTA = ?")) {
+			desc.setLong(1, agencia);
+			desc.setLong(2, numero);
+			resultado = desc.executeQuery();
+			resultado.next();
+			conta.setIdUsuario(resultado.getLong("fk_id_usuario"));
+			conta.setSaldo(resultado.getBigDecimal("NR_saldo"));
+			conta.setAgencia(resultado.getShort("NR_Agencia"));
+			conta.setNumeroConta(resultado.getInt("NR_Numero_conta"));
+			conta.setLimiteCredito(resultado.getBigDecimal("NR_Limite_credito"));
+		} catch (SQLException e) {
+			throw new  RuntimeException (e.getMessage());
 		}
 		return conta;
 	}
-	
+
 	@Override
 	public Conta retornarPorIdUsuario(long idUsuario) {
 		Conta conta = new Conta();
-		try {
-		Connection con = ConnectionJDBC.abrir();
-		desc = con.prepareStatement("SELECT * FROM tb_conta WHERE fk_id_usuario = ?");
-		desc.setLong(1,idUsuario);
-		resultado = desc.executeQuery();
-		resultado.next();
-		conta.setIdUsuario(resultado.getLong("fk_id_usuario"));
-		conta.setSaldo(resultado.getBigDecimal("NR_saldo"));
-		conta.setAgencia(resultado.getShort("NR_Agencia"));
-		conta.setNumeroConta(resultado.getInt("NR_Numero_conta"));
-		conta.setLimiteCredito(resultado.getBigDecimal("NR_Limite_credito"));
-		desc.close();
-		con.close();
-		}
-		catch(SQLException e) {
-			e.getMessage();
+		try (Connection con = ConnectionJDBC.abrir();
+				PreparedStatement desc = con.prepareStatement("SELECT * FROM tb_conta WHERE fk_id_usuario = ?")) {
+			desc.setLong(1, idUsuario);
+			resultado = desc.executeQuery();
+			resultado.next();
+			conta.setIdUsuario(resultado.getLong("fk_id_usuario"));
+			conta.setSaldo(resultado.getBigDecimal("NR_saldo"));
+			conta.setAgencia(resultado.getShort("NR_Agencia"));
+			conta.setNumeroConta(resultado.getInt("NR_Numero_conta"));
+			conta.setLimiteCredito(resultado.getBigDecimal("NR_Limite_credito"));
+		} catch (SQLException e) {
+			throw new RuntimeException(e.getMessage());
 		}
 		return conta;
 	}
