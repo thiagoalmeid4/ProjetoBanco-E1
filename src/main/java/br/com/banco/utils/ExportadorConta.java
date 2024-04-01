0bf895342a9aefa1e8d0a6a204d80c6409bff505
victor9m <victormontezani9@gmail.com>
2024-04-01 14:33:54 -0300

    dados formatados

--- src/main/java/br/com/banco/utils/ExportadorConta.java
+++ src/main/java/br/com/banco/utils/ExportadorConta.java
@@ -13,12 +13,24 @@ public class ExportadorConta {
 	public static void exportarParaTxt(List<Conta> contas) {
 	try (BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\ypiovarczik\\Downloads\\teste\\teste.txt", true))) {
 		for (Conta conta : contas) {
+		
+		String idConta=String.format("%05d", conta.getIdConta());
+		String idUsuario=String.format("%05d", conta.getIdUsuario());
+		String numeroConta=String.format("%08d", conta.getNumeroConta());
+		String agencia=String.format("%04d", conta.getAgencia());
+		String saldo= String.format("%09.2f", (double) conta.getSaldo());
+		String limiteCredito= String.format("%09.2f" (double), conta.getLimiteCredito());
+		
+			
+			
+			
+			
 		writer.write(conta.getIdConta() 
-		+ "-" + conta.getIdUsuario() 
-		+ "-" + conta.getNumeroConta()
-		+ "-" + conta.getAgencia()
-		+ "-" + conta.getSaldo()
-		+ "-" + conta.getLimiteCredito());
+		+ "-" + idUsuario
+		+ "-" + numeroConta
+		+ "-" + agencia
+		+ "-" + saldo
+		+ "-" + limiteCredito);
 		writer.newLine();
 		}
 		} catch (IOException e) {
