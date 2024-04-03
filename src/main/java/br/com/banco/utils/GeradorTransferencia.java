7fc538c8ef5f123081e2271285515eaa6a47f934
Yago Piovarczik <ypiovarczik@foursys.com.br>
2024-04-03 15:38:57 -0300

    Criado o método limparTxt

--- src/main/java/br/com/banco/utils/GeradorTransferencia.java
+++ src/main/java/br/com/banco/utils/GeradorTransferencia.java
@@ -38,7 +38,7 @@ public class GeradorTransferencia {
 	public void salvarTransferencias(Transferencia trans) {
 			try (BufferedWriter writer = new BufferedWriter(new FileWriter(
 					"C:\\Users\\vmontezani\\Documents\\Projetos\\ProjetoEquipe1\\TransferenciasGeradas.txt", true))) {
-				writer.write(cpf + "" + listaAgencia.get(indexOrigem) + ""
+				writer.write(listaCpf.get(indexOrigem) + "" + listaAgencia.get(indexOrigem) + ""
 						+ listaNumeroConta.get(indexOrigem)+ "" + listaAgencia.get(indexDestino)
 						+ "" + listaNumeroConta.get(indexDestino) + "" +valorFormatado + ""
 						+ trans.getData());
@@ -54,6 +54,7 @@ public class GeradorTransferencia {
 		for(Conta conta :cdi4.listarTodos()) {
 			listaConta.add(conta);
 		}
+		limparTxt();
 		for (int i = 0; i < i1; i++) {
 			Transferencia trans = new Transferencia();
 			acessarDados();
@@ -67,6 +68,16 @@ public class GeradorTransferencia {
 			trans.setData(geradorData());
 			salvarTransferencias(trans);
 		}
+	}
+
+	private void limparTxt() {
+			try (BufferedWriter writer = new BufferedWriter(new FileWriter(
+					"C:\\Users\\vmontezani\\Documents\\Projetos\\ProjetoEquipe1\\TransferenciasGeradas.txt", false))) {
+				writer.write("");
+				writer.flush();
+			}catch (IOException e) {
+				e.getMessage();
+			}
 	}
 
 	public LocalDateTime geradorData() {
@@ -97,4 +108,6 @@ public class GeradorTransferencia {
 			e.printStackTrace();
 		}
 	}
+	
+	
 }
